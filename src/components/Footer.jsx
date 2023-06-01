7d501750b74299e2f3dc1a9c6f1457a9879ff6fc
jose <jagonzalez.alumno@iesrafaelalberti.es>
2023-06-01 19:10:55 +0200

    responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,27 +11,36 @@ const Footer = () => {
         <Sfooter>
             <SLink to={"#"}>Terminos y condiciones</SLink>
             <SLink to={"#"}>Cookies</SLink>
-            <div>
+            <Sdiv>
                 <SLink className="icono" to={"#"}><FaInstagram /></SLink>
                 <SLink className="icono" to={"#"}><FaFacebook /></SLink>
                 <SLink className="icono" to={"#"}><FaTwitter /></SLink>
-            </div>
+            </Sdiv>
         </Sfooter>
     )
 }
 
 const Sfooter = styled.footer`
-
   display: flex;
   justify-content: space-around;
   border-top: 1px solid #000000;
   padding: 22.5px 35px;
   background-color: #ECF0F1;
 
-`;
+  @media (width <= 600px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+const Sdiv = styled.div`
+  @media (width <= 600px) {
+    margin: 20px;
+  }
+`
 
 const SLink = styled(Link)`
-
   text-decoration: none;
   color: black;
   font-size: 16px;
@@ -41,6 +50,12 @@ const SLink = styled(Link)`
     margin-left: 7px;
   }
 
+  @media (width <= 600px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 20px;
+  }
 `
 
 export default  Footer
