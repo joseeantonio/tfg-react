7a0ab2de5acf6f3fae53bc3e0457772a48b2f6be
jose <jagonzalez.alumno@iesrafaelalberti.es>
2023-06-13 12:27:21 +0200

    filtro tipo anillo

--- src/pages/TypeJewel.jsx
+++ src/pages/TypeJewel.jsx
@@ -14,10 +14,12 @@ import ButtonSubmit from "../components/ButtonSubmit";
 
 const TypeJewel = () => {
 
+    //Cogemos el tipo a traves de la url
     const {type} = useParams()
 
-
+    // Titulo de la pagina
     const [title,setTitle] = useState()
+    // Todos los productos de esa categoria
     const [jewerly,setJewerly] = useState([])
     const [filters, setFilters] = useState([]);
     const [filterJewels,setFilterJewels] = useState([])
@@ -26,28 +28,57 @@ const TypeJewel = () => {
 
     //Comprobamos si esta cada opcion en la lista de filtros y añadimos los filtros con esas opciones
     // a la una lista en variable y cuando acabe de recorrerlo le da el valor a la lista de productos filtrados.
+    // Segun el tipo de joya que sea.
     const activeFilters = () => {
-        let filterProducts = jewerly.filter((jewel) =>
-            (filters.includes("34 mm") && jewel.talla === 34) ||
-            (filters.includes("39 mm") && jewel.talla === 39) ||
-            (filters.includes("42 mm") && jewel.talla === 42) ||
-            (filters.includes("No") && jewel.resistenteAgua === 0) ||
-            (filters.includes("100 m") && jewel.resistenteAgua === 100) ||
-            (filters.includes("200 m") && jewel.resistenteAgua === 200) ||
-            (filters.includes("Mas de 200 m") && jewel.resistenteAgua > 200) ||
-            (filters.includes("Nylon") && jewel.materialCorrea === "nylon") ||
-            (filters.includes("Cuero") && jewel.materialCorrea === "cuero") ||
-            (filters.includes("Acero inoxidable") && jewel.materialCaja === "acero inoxidable") ||
-            (filters.includes("Plata") && jewel.materialCaja === "plata") ||
-            (filters.includes("Bronce") && jewel.materialCaja === "bronce") ||
-            (filters.includes("Mecanico") && jewel.tipoMovimiento === "mecanico") ||
-            (filters.includes("Cuarzo") && jewel.tipoMovimiento === "cuarzo") ||
-            (filters.includes("Plata") && jewel.tipoMovimiento === "plata")
-        )
-        setFilterJewels(filterProducts)
+        if (type === "reloj") {
+            let filterProducts = jewerly.filter((jewel) =>
+                (filters.includes("34 mm") && jewel.talla === 34) ||
+                (filters.includes("39 mm") && jewel.talla === 39) ||
+                (filters.includes("42 mm") && jewel.talla === 42) ||
+                (filters.includes("No") && jewel.resistenteAgua === 0) ||
+                (filters.includes("100 m") && jewel.resistenteAgua === 100) ||
+                (filters.includes("200 m") && jewel.resistenteAgua === 200) ||
+                (filters.includes("Mas de 200 m") && jewel.resistenteAgua > 200) ||
+                (filters.includes("Nylon") && jewel.materialCorrea === "nylon") ||
+                (filters.includes("Cuero") && jewel.materialCorrea === "cuero") ||
+                (filters.includes("Acero inoxidable") && jewel.materialCaja === "acero inoxidable") ||
+                (filters.includes("Plata") && jewel.materialCaja === "plata") ||
+                (filters.includes("Bronce") && jewel.materialCaja === "bronce") ||
+                (filters.includes("Mecanico") && jewel.tipoMovimiento === "mecanico") ||
+                (filters.includes("Cuarzo") && jewel.tipoMovimiento === "cuarzo") ||
+                (filters.includes("Plata") && jewel.tipoMovimiento === "plata")
+            )
+            setFilterJewels(filterProducts)
+        } else if (type === "anillo") {
+            let filterProducts = jewerly.filter((jewel) =>
+                (filters.includes("60 - 62 mm") && jewel.talla <=62 && jewel.talla >=60) ||
+                (filters.includes("64 - 66 mm") && jewel.talla <=66 && jewel.talla >=64) ||
+                (filters.includes("68 - 70 mm") && jewel.talla <=67 && jewel.talla >=68) ||
+                (filters.includes("Azul") && jewel.color === "azul") ||
+                (filters.includes("Amarillo") && jewel.color === "amarillo") ||
+                (filters.includes("Negro") && jewel.color === "negro") ||
+                (filters.includes("Verde") && jewel.color === "verde") ||
+                (filters.includes("Rojo") && jewel.color === "rojo") ||
+                (filters.includes("9 gramos") && jewel.peso === "9 gramos") ||
+                (filters.includes("11 gramos") && jewel.peso === "11 gramos") ||
+                (filters.includes("14 gramos") && jewel.peso === "14 gramos") ||
+                (filters.includes("14k") && jewel.calidadMinima === "14K") ||
+                (filters.includes("18k") && jewel.calidadMinima === "18K") ||
+                (filters.includes("24k") && jewel.calidadMinima === "24K") ||
+                (filters.includes("Diamante") && jewel.piedra === "diamante") ||
+                (filters.includes("Moissanita") && jewel.piedra === "moissanita") ||
+                (filters.includes("Zafiro") && jewel.piedra === "zafiro") ||
+                (filters.includes("Esmeralda") && jewel.piedra === "esmeralda") ||
+                (filters.includes("1") && jewel.numeroPiedra === 1) ||
+                (filters.includes("2") && jewel.numeroPiedra === 2) ||
+                (filters.includes("3") && jewel.numeroPiedra === 3) ||
+                (filters.includes("5") && jewel.numeroPiedra === 5)
+            )
+            setFilterJewels(filterProducts)
+        }
     }
 
-
+    // Ponemos el titulo de la pagina en plural
     const plural = () => {
         if (type === "anillo") {
             setTitle("ANILLOS")
@@ -58,6 +89,7 @@ const TypeJewel = () => {
         }
     }
 
+    // Llamada a api, para coger todos los productos de ese tipo de la api
     const fetchDataApi = async () => {
         try {
             const result = await petition(`/productos/tipo/${type}`)
